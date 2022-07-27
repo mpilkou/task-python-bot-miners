938d0ae7a4da41d5d9f329a84649cf59b795d3d5
mpilkou <mpilkou@gmail.com>
2022-07-27 22:17:15 +0300

    rename env file & variable

--- miners_list_bot.py
+++ miners_list_bot.py
@@ -4,7 +4,7 @@ import parser
 
 from dotenv import dotenv_values
 
-config = dotenv_values('env_variables')
+config = dotenv_values('variables')
 SECRET_KEY = config['SECRET_KEY']
 
 bot = telebot.TeleBot(SECRET_KEY)
@@ -16,16 +16,16 @@ def start_message(message):
 @bot.message_handler(commands = ['update'])
 def update_message(message):
 
-    miners_iteratorable = list(set(parser.get_miners_list()))
+    miners_list = list(set(parser.get_miners_list()))
     miners_per_message = 100
-    messages_number = len(miners_iteratorable) // miners_per_message
+    messages_number = len(miners_list) // miners_per_message
 
     for message_number in range(messages_number):
         miners_from = message_number * miners_per_message
         miners_to = miners_from + miners_per_message + 1
-        bot.send_message(message.chat.id, '\n'.join(miners_iteratorable[miners_from: miners_to]))
+        bot.send_message(message.chat.id, '\n'.join(miners_list[miners_from: miners_to]))
 
-    bot.send_message(message.chat.id, '\n'.join(miners_iteratorable[len(miners_iteratorable) % miners_per_message :]))
+    bot.send_message(message.chat.id, '\n'.join(miners_list[len(miners_list) % miners_per_message :]))
 
 
 
